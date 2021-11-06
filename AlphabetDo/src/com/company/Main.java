921844284a53151b4680c8d4086a5ce81feedc36
Vlad <ehedz@mail.ru>
2021-11-06 13:13:58 +0300

    t-17

---
+++ AlphabetDo/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char ch = 'я';
+        do {
+            System.out.println(ch);
+            ch--;
+        } while (ch >= 'А');
+    }
+}
